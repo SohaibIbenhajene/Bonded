4fc21df4ce291e6f8d32aff92914cee428e8c867
Sohaib Ibenhajene <91062358+SohaibIbenhajene@users.noreply.github.com>
2025-01-22 21:13:25 +0100

    Basics of the spring boot backend are complete

---
+++ src/main/java/dev/sohaib/bonded/BondedApplication.java
@@ -0,0 +1,13 @@
+package dev.sohaib.bonded;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BondedApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BondedApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/sohaib/bonded/BondedApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.sohaib.bonded;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BondedApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
